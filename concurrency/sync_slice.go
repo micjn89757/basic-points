99941f93f94f3a642cbeb8e142dfb020b61859e6
djn <1980846456@qq.com>
2024-08-01 00:07:26 +0800

    update pool

--- concurrency/sync_slice.go
+++ concurrency/sync_slice.go
@@ -1,1 +1,42 @@
 package concurrency
+
+/*
+并发安全的slice
+TODO
+*/
+
+type Sig uint8
+
+const (
+	add Sig = iota 
+	sub 
+)
+
+type SyncSlice[T any] struct{
+	ch chan Sig
+	d chan T
+	data []T
+}
+
+
+func NewSlice[T any](length int, cap int) *SyncSlice[T] {
+	return &SyncSlice[T]{
+		ch: make(chan Sig, 3),
+		d: make(chan T, 3),
+		data: make([]T, length, cap),
+	}
+}
+
+func (s *SyncSlice[T]) handler() {
+	select {
+	case sig := <- s.ch:
+		switch sig {
+		case add:
+			s.data = append(s.data, )
+		}
+	}
+}
+
+func (s *SyncSlice[T]) Add(ele T) {
+	s.ch <- add
+}
